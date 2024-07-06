74bedb25ed4a8354d3204508923b57af9dd2bff0
zend-02 <abdillahiabdourahman65@gmail.com>
2024-07-06 17:34:53 +0300

    fusion deux partie

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\BaseContent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,14 +19,21 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [BaseContent::class, 'welcome'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // partie develloper avec breeze-laravel
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    // partie develloper sans librairi
+    Route::get('/vigetteObetnu/{id}',[BaseContent::class,'vigetteObetnu'])->name('vigetteObetnu');
+    Route::get('/achatVignette',[BaseContent::class,'achatVignette'])->name('achatVignette');
+    Route::get('/welcome',[BaseContent::class,'welcome'])->name('welcome');
+    Route::get('/service',[BaseContent::class,'service'])->name('service');
+    Route::get('/profile',[BaseContent::class,'profile'])->name('profile');
+    Route::get('/vignette/{id}',[BaseContent::class,'vignette'])->name('vignette');
 });
 
 require __DIR__.'/auth.php';
